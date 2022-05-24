ba2e68c00fe9542dc5803fcb879a87946cd5cd3a
Ivan-Yuliyanov-Atanasov <88737035+Ivan-Yuliyanov-Atanasov@users.noreply.github.com>
2022-05-24 09:06:37 +0300

    Create employee component
    Some changes also to employee service, routing config

--- frontend/src/app/employee.service.ts
+++ frontend/src/app/employee.service.ts
@@ -15,4 +15,8 @@ export class EmployeeService {
   getEmployeesList(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.baseURL}`);
   }
+
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.http.post(`${this.baseURL}`, employee);
+  }
 }
