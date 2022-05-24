a8554ecf64baca6f1ea3b6890a37001bec0c7837
Ivan-Yuliyanov-Atanasov <88737035+Ivan-Yuliyanov-Atanasov@users.noreply.github.com>
2022-05-24 09:28:43 +0300

    Get employee by Id endpoint created

--- backend/src/main/java/com/example/employeecrudapp/controller/EmployeeController.java
+++ backend/src/main/java/com/example/employeecrudapp/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.employeecrudapp.controller;
 
 
+import com.example.employeecrudapp.exception.ResourceNotFoundException;
 import com.example.employeecrudapp.model.Employee;
 import com.example.employeecrudapp.repository.EmployeeRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +28,12 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return this.employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
     }
 }
