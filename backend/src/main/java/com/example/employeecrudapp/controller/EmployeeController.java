12dbf4148dc68c8c97af8e7789953703d8b0306a
Ivan-Yuliyanov-Atanasov <88737035+Ivan-Yuliyanov-Atanasov@users.noreply.github.com>
2022-05-24 09:03:12 +0300

    Added create employee endpoint

--- backend/src/main/java/com/example/employeecrudapp/controller/EmployeeController.java
+++ backend/src/main/java/com/example/employeecrudapp/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.employeecrudapp.controller;
 
 import com.example.employeecrudapp.model.Employee;
 import com.example.employeecrudapp.repository.EmployeeRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return this.employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return this.employeeRepository.save(employee);
     }
 }
